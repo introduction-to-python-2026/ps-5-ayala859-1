10b6ec3be2bcf28c5189acc8815f83aeba1a0583
ayala859 <ayalla.goren@gmail.com>
2025-12-02 22:17:32 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -30,18 +30,17 @@ def split_at_digit(formula):
         numeric = int(formula[digit_location:])
         return (perfix, numeric)
 def count_atoms_in_molecule(molecular_formula):
-    """Takes a molecular formula (string) and returns a dictionary of atom counts.  
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
 
-    # Step 1: Initialize an empty dictionary to store atom counts
-
-    for atom in split_by_capitals(molecular_formula):
-        atom_name, atom_count = split_at_number(atom)
+    atom_counts = {}
+    list=split_before_uppercases(molecular_formula)
+    for atom in list:
+        atom_name, atom_count = split_at_digit(atom)
         
-        # Step 2: Update the dictionary with the atom name and count
 
-    # Step 3: Return the completed dictionary
+        atom_counts[atom_name] = atom_count
+    
 
+    return atom_counts
 
 
 def parse_chemical_reaction(reaction_equation):
