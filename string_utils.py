5d1841bd6b8a5001c3029953cce4c96f1af8497b
ayala859 <ayalla.goren@gmail.com>
2025-12-06 18:33:00 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,18 +1,17 @@
 def split_before_uppercases(formula):
+    if formula == "":
+        return []
+
     start = 0
     split_formula = []
-    
+
     for end in range(1, len(formula)):
-        if formula[end].isupper():     
-            x = formula[start:end]     
-            split_formula.append(x)
-            start = end                
-    
-    if formula != "":
-        split_formula.append(formula[start:])
-        return split_formula
-    else:
-        return []
+        if formula[end].isupper():
+            split_formula.append(formula[start:end])
+            start = end
+
+    split_formula.append(formula[start:])
+    return split_formula
 
 
 def split_at_digit(formula):
@@ -21,25 +20,26 @@ def split_at_digit(formula):
         if char.isdigit():
             break
         digit_location += 1
+
     if digit_location == len(formula):
         return (formula, 1)
     else:
-        perfix = formula[:digit_location]
+        prefix = formula[:digit_location]
         numeric = int(formula[digit_location:])
-        return (perfix, numeric)
-
-
+        return (prefix, numeric)
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.
-    Example: 'H2O' → {'H': 2, 'O': 1}"""
-    atom_counts = {}
-    list=split_before_uppercases(molecular_formula)
-    for atom in list:
-        atom_name, atom_count = split_at_digit(atom)
-        if atom_name in atom_counts:
-            atom_counts[atom_name] += atom_count
-        else:
-            atom_counts[atom_name] = atom_count
-    return atom_counts
+    Example: 'H2O' -> {'H': 2, 'O': 1}"""
+    dict_atom_counts = {}
+    list1 = split_before_uppercases(molecular_formula)
+    print(list1)
+    for item in list1:
+        k = split_at_digit(item)
+        dict_atom_counts[k[0]] = k[1]
 
+    return dict_atom_counts
+
+
+formula = "H2C"
+print(count_atoms_in_molecule(formula))
